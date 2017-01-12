f592dff4221b6a3a3d9f89a6d96325940095ee70
Marcin Ziąbek <marcin.ziabek@uj.edu.pl>
2017-01-12 18:50:47 +0100

    redesign of the tester module

--- classifier.py
+++ classifier.py
@@ -1,20 +1,84 @@
 from voice import Voice
 from sample import Sample
 
-from settings import VOICES_NUMBER, NUMBER_OF_SAMPLES, SAMPLES_TO_LEARN
+from settings import NUMBER_OF_VOICES, NUMBER_OF_SAMPLES, SAMPLES_TO_LEARN
+from settings import DECOMPOSITION_ALGORITHM, DECOMPOSITION_COMPONENTS, CLASSIFIER_ALGORITHM
+from settings import LOG_ACTIONS_TO_CONSOLE
+
+class Tester():
+
+	def __init__(self):
+		self.voices = []
+		self.analyser = DECOMPOSITION_ALGORITHM(n_components=DECOMPOSITION_COMPONENTS)
+		self.classifier = CLASSIFIER_ALGORITHM()
+
+		self.load_voices()
+		self.load_samples()
+		self.learn()
+
+	def log_action(self, text):
+		if LOG_ACTIONS_TO_CONSOLE:
+			print(str(text))
+	
+	def load_voices(self):
+		self.log_action("loading {0} voices".format(NUMBER_OF_VOICES))
+
+		for i in range(1, NUMBER_OF_VOICES+1):
+			voice = Voice(i)
+			self.voices += [voice]
+
+	def load_samples(self):
+		self.log_action("loading samples")
+
+		for voice in self.voices:
+			self.log_action("{0}".format(voice.id))
+
+			voice.read_samples()
+
+	def learn(self):
+		self.log_action("learning")
+
+		data = []
+		target = []
+
+		for voice in self.voices:
+			self.log_action(voice.id)
+
+			samples_to_learn = voice.samples[0:SAMPLES_TO_LEARN]
+			voice_features = []
+
+			for sample in samples_to_learn:
+				data += [sample.features]
+				target += [voice.id]
+
+	def get_predictions(self, analysis_function):
+		self.log_action("getting predictions")
+
+		for voice in voices:
+			self.log_action(voice.id)
+
+			voice_features = []
+
+			for sample in voice.samples[SAMPLES_TO_LEARN:(NUMBER_OF_VOICES+1)]:
+				voice_features += [sample.features]
+	
+			voice_features = pca.transform(voice_features)
+			res = knn.predict_proba(voice_features)
+
+			for r in res:
+				res_d = []
+
+
 
 
 print("Preparing voices and analysis")
 
-voices = []
+voices = Tester().voices
 
-for i in range(1, VOICES_NUMBER+1):
-	print(i)
 
-	voice = Voice(i)
-	voice.read_samples()
+	
 
-	voices += [voice]
+	
 
 
 # ===========================================================================
@@ -35,14 +99,12 @@ for voice in voices:
 		target += [voice.id]
 
 
-from sklearn.decomposition import PCA, FactorAnalysis, FastICA, TruncatedSVD
-pca = FastICA(n_components=vector_components)
+pca = DECOMPOSITION_ALGORITHM(n_components=DECOMPOSITION_COMPONENTS)
 pca.fit(data)
 data = pca.transform(data)
 
 
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier()
+knn = CLASSIFIER_ALGORITHM()
 knn.fit(data, target)
 
 
@@ -56,6 +118,8 @@ ok_2 = 0
 ok_3 = 0
 ok_4 = 0
 ok_5 = 0
+probability = 0
+selected = 0
 all = 0
 
 for voice in voices:
@@ -63,7 +127,7 @@ for voice in voices:
 
 	voice_features = []
 
-	for sample in voice.samples[SAMPLES_TO_LEARN:(NUMBER_OF_SAMPLES+1)]:
+	for sample in voice.samples[SAMPLES_TO_LEARN:(NUMBER_OF_VOICES+1)]:
 		voice_features += [sample.features]
 	
 	voice_features = pca.transform(voice_features)
@@ -71,6 +135,9 @@ for voice in voices:
 
 	for r in res:
 		res_d = []
+
+		probability += r[voice.id-1]
+		selected += len([s for s in r if s!=0])
 
 		for i in range(0, len(r)):
 			res_d += [{
@@ -97,6 +164,8 @@ for voice in voices:
 		
 		all += 1
 
+print("probability: {0} %".format(probability*100.0/all))
+print("selected: {0}".format(selected/all))
 print("{0} / {1}: {2} %".format(ok_1, all, ok_1*100.0/all))
 print("{0} / {1}: {2} %".format(ok_2, all, ok_2*100.0/all))
 print("{0} / {1}: {2} %".format(ok_3, all, ok_3*100.0/all))
--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ CACHE_PATH = CURRENT_PATH + "/cache"
 
 """ DATABASE OF VOICES """
 
-VOICES_NUMBER = 25 # number of voices in the database
+NUMBER_OF_VOICES = 25 # number of voices in the database
 NUMBER_OF_SAMPLES = 100 # how many samples has each of voices
 
 
@@ -28,5 +28,17 @@ APPLY_LOG_POWER = True # changes FFT energy to dB
 
 """ LEARNING """
 
-SAMPLES_TO_LEARN = 10 # number of samples to learn
-RECOGNITION_COMPONENTS = 32 # length of the vector that describes each of samples
+from sklearn.decomposition import PCA, FactorAnalysis, FastICA, TruncatedSVD
+from sklearn.neighbors import KNeighborsClassifier
+
+DECOMPOSITION_ALGORITHM = FastICA # algorithm used for simplify FFT vector into shorter vector of components
+DECOMPOSITION_COMPONENTS = 32 # length of the vector that describes each of samples
+
+CLASSIFIER_ALGORITHM = KNeighborsClassifier # algorithm used for classify samples as appropriate voices
+
+SAMPLES_TO_LEARN = 5 # number of samples to learn
+
+
+""" OTHER """
+
+LOG_ACTIONS_TO_CONSOLE = True # prints current actions to the console
